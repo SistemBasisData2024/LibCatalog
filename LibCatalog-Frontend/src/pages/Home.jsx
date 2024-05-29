657d76e833f2f2160ad6f324702144fbccc9c63d
Christopher Sutandar <144262642+christopherSuts@users.noreply.github.com>
2024-05-29 20:29:22 +0700

    top5book

--- LibCatalog-Frontend/src/pages/Home.jsx
+++ LibCatalog-Frontend/src/pages/Home.jsx
@@ -6,20 +6,67 @@ import { Link, Route, Router } from "react-router-dom";
 
 const Home = () => {
     const [backendData, setBackendData] = useState([{}]);
+    const [topBooks, setTopBooks] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
         fetch("http://localhost:5000/home")
-        .then((response) => response.json())
-        .then((data) => setBackendData(data))
-        .catch((err) => console.log(err));
+            .then((response) => response.json())
+            .then((data) => setBackendData(data))
+            .catch((err) => console.log(err));
+
+        fetch("http://localhost:5000/home/top")
+            .then((response) => response.json())
+            .then((data) => setTopBooks(data))
+            .catch((err) => console.log(err));
     }, []);
     
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % topBooks.length);
+        }, 3000); // Change book every 3 seconds
+
+        return () => clearInterval(interval);
+    }, [topBooks.length]);
+
     return (
         <div>
             <Navbar />
+
+            <div className="top-books-container">
+                <div
+                    className="top-books-slider"
+                    style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+                >
+                {topBooks.length > 0 ? (
+                    topBooks.map((book, index) => (
+                        <div key={index} className="top-book">
+                            <img src={book.cover} alt={book.judul} />
+                            <div className="book-info">
+                                <h2>{book.judul}</h2>
+                                <p>{book.deskripsi}</p>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p className="loading-message">Loading top books...</p>
+                )}
+                </div>
+            </div>
+
             <h1 className='allBooks'>BOOK CHOICES</h1>
             <p className='allBooks'>This Lorem Ipsum was written manually Ohio sigma fanum tax 1550s</p>
             
+            <div class="button-container">
+                <button className='genreButton'>All Books</button>
+                <button className='genreButton'>Fiction</button>
+                <button className='genreButton'>Romance</button>
+                <button className='genreButton'>Novel</button>
+                <button className='genreButton'>Science</button>
+                <button className='genreButton'>Science-fiction</button>
+                <button className='genreButton'>Motivation</button>
+            </div>
+
             <div className='book-list'>
                 {backendData.length > 0 ? (
                     backendData.map((book, index) => (
