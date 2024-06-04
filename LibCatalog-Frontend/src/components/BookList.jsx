cf415c2ea788d8e5c04738b78dd90393fc6d3134
Christopher Sutandar <144262642+christopherSuts@users.noreply.github.com>
2024-06-05 02:38:46 +0700

    ganti css ke tailwind dikit

--- LibCatalog-Frontend/src/components/BookList.jsx
+++ LibCatalog-Frontend/src/components/BookList.jsx
@@ -53,11 +53,16 @@ const BookList = () => {
     return (
         <div>
             <ToastContainer />
-            <h1 className='allBooks'>BOOK CHOICES</h1>
-            <p className='allBooks'>This Lorem Ipsum was written manually Ohio sigma fanum tax 1550s</p>
+            <h1 className='text-center text-[23px] text-[black] font-[bold] mt-5'>BOOK CHOICES</h1>
+            <p className='text-center text-base text-[black] mb-5'>This Lorem Ipsum was written manually Ohio sigma fanum tax 1550s</p>
             
-            <div className="button-container">
-                <button className={`genreButton ${selectedGenre === '' ? 'active' : ''}`} 
+            <div className="flex justify-center items-center gap-[15px]">
+                <button className={`${selectedGenre === '' ? 
+                    'bg-[#CBB595] font-[bold] text-[black] text-center no-underline inline-block text-sm mx-0.5 my-1 p-3.5 rounded-xl border-[1.5px] border-solid border-[black] scale-110'
+                    // 'bg-[#CBB595] font-[bold] text-center no-underline inline-block text-sm mx-0.5 my-1 p-3.5 rounded-xl border-[1.5px] border-solid border-[black] scale-110'
+                    : 
+                    'bg-[#fbfff1] font-[bold] text-[black] text-center no-underline inline-block text-sm transition-transform duration-[0.2s] ease-[ease-in-out] mx-0.5 my-1 p-3.5 rounded-xl border-[1.5px] border-solid border-[black] hover:bg-[#CBB595] hover:text-[black] hover:border-[1.5px] hover:border-solid hover:border-[black] hover:scale-110'
+                }`} 
                     onClick={() => 
                         fetch("http://localhost:5000/home")
                             .then(response => response.json())
@@ -111,7 +116,7 @@ const BookList = () => {
                         <div key={index} className='book-item'>
                             <Link to={`/book/${book.isbn}`} className='book-link'>
                                 <img src={book.cover} alt={book.judul} className='book-cover' />
-                                <div className='book-info'>
+                                <div className='mt-2.5 grow flex flex-col items-center mt-auto p-2.5;'>
                                     <h2 className='book-title'>{book.judul}</h2>
                                     <p className='book-description'>{book.deskripsi}</p>
                                 </div>
